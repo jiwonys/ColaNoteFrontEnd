fb5a31960c5a311e564bbb826da83e07e53a7d30
jiwon.kim989@gmail.com <jiwon.kim989@gmail.com>
2023-08-18 22:59:20 -0700

    "notes move as expected, drop point is setting to the x and y distance"

--- src/app/sticky-notes/sticky-notes.component.ts
+++ src/app/sticky-notes/sticky-notes.component.ts
@@ -16,8 +16,11 @@ export class StickyNotesComponent implements OnInit {
   @Output() positionChange = new EventEmitter<{ x: number, y: number }>();
   noteX : number = 0;
   noteY : number = 0;
-  positionsLoaded: boolean = false;
+  mouseClickX: number = 0;
+  mouseClickY: number = 0;
 
+  positionsLoaded: boolean = false;
+  noteCopy : any;
   constructor(private stickyNoteService: StickyNoteService) { }
 
   ngOnInit() {
@@ -27,7 +30,6 @@ export class StickyNotesComponent implements OnInit {
   loadStickyNotes() {
     this.stickyNoteService.getStickyNotes(1).subscribe((board) => {
       this.stickyNotes = board.notes;
-      console.log(this.stickyNotes);
     });
   }
   //fdasf
@@ -55,20 +57,33 @@ export class StickyNotesComponent implements OnInit {
     });
   }
   positionStartChange(event:any, note:any){
+  console.log(event);
+  console.log(event.event.clientX);
+  console.log(event.event.clientY);
+
+    this.mouseClickX = event.event.clientX - note.xaxis;
+    this.mouseClickY = event.event.clientY - note.yaxis;
     this.noteX = note.xaxis;
     this.noteY = note.yaxis;
-    console.log(this.noteX);
-    console.log(this.noteY);
   }
 
   onPositionChange(event: any, note: any) {
-    const distance = event.distance;
-    console.log(event);
-    note.xaxis += distance.x;
-    note.yaxis += distance.y;
-    this.updateStickyNote(note); // Update the sticky note position in the backend
+  console.log(event);
+      const dropPoint = event.dropPoint;
+          const stickyNoteWidth = 200; // The width of your sticky note
+          const stickyNoteHeight = 100; // The height of your sticky note
 
+          // Calculate the new x and y positions based on the mouse position
+          const newX = dropPoint.x;
+          const newY = dropPoint.y;
 
+          // Update the noteCopy object with the new position
+          this.noteCopy = { ...note };
+
+          this.noteCopy.xaxis = newX ;
+          this.noteCopy.yaxis = newY ;
+
+          this.updateStickyNote(this.noteCopy); // Update the sticky note position in the backend
   }
 
 }
