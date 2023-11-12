9121971df5c37b1a7a476fd1f4ed933f83b8bdd8
jiwon.kim989@gmail.com <jiwon.kim989@gmail.com>
2023-11-11 21:11:14 -0700

    Ready for initial test deployment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiurl: 'http://colanote.us-east-2.elasticbeanstalk.com',
+  apiurl: 'https://colanote.us-east-2.elasticbeanstalk.com',
   // other production-specific configurations
 };
 //
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiurl: 'http://colanote.us-east-2.elasticbeanstalk.com',
+  apiurl: 'https://colanote.us-east-2.elasticbeanstalk.com',
 //   apiurl: 'http://localhost:8080/api/v1/board',
 };
