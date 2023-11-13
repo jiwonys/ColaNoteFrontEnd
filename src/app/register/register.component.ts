69c163d5fd2b1e0a34e27810a618bbd973aaf7e5
jiwon.kim989@gmail.com <jiwon.kim989@gmail.com>
2023-11-12 18:50:13 -0700

    Email Check if valid

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -15,25 +15,37 @@ export class RegisterComponent {
   password: string = '';
   private apiUrl = environment.apiurl;
 
-
   constructor(private http: HttpClient, private router: Router) {}
 
   register() {
+    // Check email format
+    if (!this.isValidEmail()) {
+      alert('Invalid Email format!');
+      return;
+    }
+
     const body = {
       firstName: this.firstName,
       lastName: this.lastName,
       email: this.email,
       password: this.password
     };
-    this.http.post(`${this.apiUrl}` + '/api/v1/auth/register', body)
+
+    this.http.post(`${this.apiUrl}/api/v1/auth/register`, body)
       .subscribe(() => {
-      alert("Registeration Successful!")
+        alert('Registration Successful!');
         // Registration successful, redirect to login page
         this.router.navigate(['/authenticate']);
       }, (error: any) => {
-      alert("registeration failed, please populate all fields");
+        alert('Registration failed, please populate all fields');
         console.error('Registration failed:', error);
         // Handle registration error, e.g., display error message to the user
       });
+  }
+
+  private isValidEmail(): boolean {
+    // Simple email format check
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(this.email);
   }
 }
